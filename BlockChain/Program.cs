99ac7b3a8018a1733e87ab490f4ab9880c110cfb
Greg doud <gpdoud@gmail.com>
2017-09-18 09:02:20 -0400

    Added serialization to json.

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -9,22 +9,22 @@ using BlockChainLibrary;
 namespace BlockChainProgram {
 	class Program {
 		static void Main(string[] args) {
-			//BlockChain bc = new BlockChain();
-			//bc.CreateBlock(new BlockData("ABC123"));
-			//bc.CreateBlock(new BlockData("XYZ789"));
-			//bc.CreateBlock(new BlockData("Hello, world!"));
-			//bc.CreateBlock(new BlockData("This is BlockChain in action"));
-			//bc.CreateBlock(new BlockData("Received $200 from X"));
-			//bc.CreateBlock(new BlockData("Paid $100 to Y"));
-			//bc.PrintBlockChain();
-			//bc.ValidateBlockChain();
 
+			Console.WriteLine($"Start {DateTime.Now}");
 			BlockChain reg = new BlockChain();
 			reg.CreateBlock(new AccountRegisterData(0, 1000, 1000));
 			reg.CreateBlock(new AccountRegisterData(1000, -100, 900));
 			reg.CreateBlock(new AccountRegisterData(900, 500, 1400));
-			reg.PrintBlockChain();
-			reg.ValidateBlockChain();
+			Console.WriteLine($"End {DateTime.Now}");
+			//reg.PrintBlockChain();
+			//reg.ValidateBlockChain();
+			reg.Serialize();
+			reg.Dispose();
+
+			BlockChain bc = new BlockChain();
+			bc.CreateBlock(new AccountRegisterData(1400, -200, 1200));
+			bc.PrintBlockChain();
+			bc.Dispose();
 
 			Console.Write("Press any key ...");
 			Console.ReadKey();
--- BlockChainLibrary/BlockChain.cs
+++ BlockChainLibrary/BlockChain.cs
@@ -6,18 +6,35 @@ using System.Threading.Tasks;
 
 namespace BlockChainLibrary {
 
-	public class BlockChain {
+	public class BlockChain : IDisposable {
 
 		public static List<Block> Blocks;
+		private static string SerializedBlocksFileName = "blocks.bin";
 
-		public void ValidateBlockChain() {
-			string message = null;
-			foreach(var block in Blocks) {
+		public void Serialize() {
+			var BlocksSerialized = Newtonsoft.Json.JsonConvert.SerializeObject(Blocks);
+			var path = System.IO.Directory.GetCurrentDirectory();
+			var outputPath = System.IO.Path.Combine(path, SerializedBlocksFileName);
+			System.IO.File.WriteAllText(outputPath, BlocksSerialized);
+		}
+		public void Deserialize() {
+			var path = System.IO.Directory.GetCurrentDirectory();
+			var inputPath = System.IO.Path.Combine(path, SerializedBlocksFileName);
+			var BlocksSerialized = System.IO.File.ReadAllText(inputPath);
+			Blocks = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Block>>(BlocksSerialized);
+		}
+
+		public (bool, string) ValidateBlockChain() {
+			bool rc = true;
+			string msg = "Validated";
+			foreach (var block in Blocks) {
 				if(!BlockChainLibrary.Crypto.Sha256.CalcHash(block.ToHashString()).Equals(block.Hash)) {
-					message = $"Block index {block.Index} is corrupt.";
+					rc = false;
+					msg = $"Block index {block.Index} is corrupt.";
+					break;
 				}
 			}
-			Console.WriteLine(message ?? "Block chain validated.");
+			return (rc, msg);
 		}
 		public void PrintBlockChain() {
 			foreach(var block in Blocks) {
@@ -36,7 +53,10 @@ namespace BlockChainLibrary {
 			} while (!BlockChainLibrary.Crypto.Sha256.IsValidHash(block.Hash));
 			Blocks.Add(block);
 		}
-
+		private void InitializeBlocks() {
+			Blocks = new List<Block>();
+			CreateBlock(new GenesisBlock());
+		}
 		private Block GetLastBlock() {
 			if (Blocks.Count == 0) {
 				return null;
@@ -45,8 +65,45 @@ namespace BlockChainLibrary {
 			}
 		}
 		public BlockChain() {
-			Blocks = new List<Block>();
-			CreateBlock(new GenesisBlock());
+			var path = System.IO.Directory.GetCurrentDirectory();
+			var inputPath = System.IO.Path.Combine(path, SerializedBlocksFileName);
+			if (System.IO.File.Exists(inputPath)) {
+				Deserialize();
+			} else {
+				InitializeBlocks();
+			}
 		}
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing) {
+			if (!disposedValue) {
+				if (disposing) {
+					// dispose managed state (managed objects).
+					Serialize();
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~BlockChain() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose() {
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
 	}
 }
