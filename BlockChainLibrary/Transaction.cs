a4946879885f44649f974658f6c198bd4f91c61f
Greg doud <gpdoud@gmail.com>
2017-11-20 09:39:38 -0500

    Added Transaction and TransactionDetail

---
+++ BlockChainLibrary/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainLibrary {
+	class Transaction {
+
+		public string TxId { get; set; } // transaction id
+		public string Version { get; set; } // transaction version number
+		public IDictionary<int, TransactionDetail> Input { get; set; } // transaction output
+		public IDictionary<int, TransactionDetail> Output { get; set; } // transaction output
+
+		public Transaction() {
+			Input = new Dictionary<int, TransactionDetail>();
+			Output = new Dictionary<int, TransactionDetail>();
+		}
+	}
+}
